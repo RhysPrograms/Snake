4ca44dc91859b9abab2c107fe4ce41ac936293a0
RhysPrograms <rhysatkinson@protonmail.com>
2024-05-31 11:34:35 +0100

    SNK-5: Generating food, handling keypresses, & adding movement

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Define the HTML elements
 const board = document.querySelector(".game-board");
+const instructions = document.querySelector("#instruction-text");
+const logo = document.querySelector("#logo");
 
 // Define the game variables
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = "right";
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
 
 // Draw the game map, snake, and food
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
 // Draw the snake
@@ -28,8 +37,93 @@ function createGameElement(tag, className) {
 
 // Set the position of the snake, or the food
 function setPosition(element, position) {
-  element.style.gridColumn = position.x; // Horizontal
-  element.style.gridRow = position.y; // Vertical
+  element.style.gridColumnStart = position.x; // Horizontal
+  element.style.gridRowStart = position.y; // Vertical
 }
 
-draw();
+// Draw food function
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+// Generating the Food
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "right":
+      head.x++;
+      break;
+  }
+
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    clearInterval();
+    gameInterval = setInterval(() => {
+      move();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+// setInterval(() => {
+//   move();
+//   draw();
+// }, 200);
+
+function startGame() {
+  gameStarted = true;
+  instructions.style.display = "none";
+  logo.style.display = "none";
+  gameInterval = setInterval(() => {
+    move();
+    // checkCollision();
+    draw();
+  }, gameSpeedDelay);
+}
+
+//draw();
+
+// Keypress Event Listener
+function handleKeyPress(event) {
+  if (!gameStarted && event.code === "Space") {
+    startGame();
+  } else {
+    switch (event.key) {
+      case "ArrowUp":
+        direction = "up";
+        break;
+      case "ArrowDown":
+        direction = "down";
+        break;
+      case "ArrowLeft":
+        direction = "left";
+        break;
+      case "ArrowRight":
+        direction = "right";
+        break;
+    }
+  }
+}
+
+document.addEventListener("keydown", handleKeyPress);
